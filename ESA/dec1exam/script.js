cf645d3f0e6bc5f68f677379a0a8a1cc7bc413ac
Mohamad-Kazem Abd-el-Sater <mkhas313@gmail.com>
2023-12-01 14:47:49 +0200

    Add highlight functionality - ESA Dec 1 exam end of time limit

--- ESA/dec1exam/script.js
+++ ESA/dec1exam/script.js
@@ -3,7 +3,7 @@ let getItemTemplate = (name, type) => {
 	let pre = type == "fruit" ? "Fruits" : "Legumes";
 
 	return `				
-  			<li class="list-group-item bg-primary px-3 border-0 rounded-3 mb-2 ${tag}">
+  			<li class="list-group-item px-3 border-0 rounded-3 mb-2 ${tag}">
 					${pre}! - ${name} 
 				</li>`;
 };
@@ -16,9 +16,35 @@ const toggleAddItemType = (value) => {
 
 let addGeneralBtn = document.getElementById("addGeneralBtn");
 let combinedList = document.getElementById("combinedList");
+let addSpecificBtn = document.getElementById("addSpecificBtn");
 
 //Add event Listener on General Button
 addGeneralBtn.addEventListener("click", () => {
 	let name = document.getElementById("inputBar").value;
+	if (!name) {
+		alert("Please Enter a name");
+		return;
+	}
 	combinedList.innerHTML += getItemTemplate(name, itemAddType);
+});
+
+//Add event Listener on Specific Button
+addSpecificBtn.addEventListener("click", () => {
+	let name = document.getElementById("inputBar").value;
+	if (!name) {
+		alert("Please Enter a name");
+		return;
+	}
+	document.getElementById(`${itemAddType}sList`).innerHTML += getItemTemplate(
+		name,
+		itemAddType
+	);
+});
+
+//highlight selected
+const listEntries = document.querySelectorAll("li");
+listEntries.forEach((item) => {
+	item.addEventListener("click", () => {
+		item.style.backgroundColor = "#dc3545";
+	});
 });
