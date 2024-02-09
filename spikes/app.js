319f294d8b379f378c9c688e4ee11669e08aad12
Mohamad-Kazem Abd-el-Sater <mkhas313@gmail.com>
2024-02-09 16:55:47 +0200

    Add mongo connection function in app.js and finalize connection to the server

--- spikes/app.js
+++ spikes/app.js
@@ -2,13 +2,18 @@ const dotenv = require("dotenv").config();
 const express = require("express");
 const expressLayout = require("express-ejs-layouts");
 
+const connectDB = require("./server/config/db");
+
 const app = express();
 const PORT = 5000 || process.env.PORT;
+
+// Connect to DB
+connectDB();
 
 // static is serving the public folder allowing us to use it from our ejs files
 app.use(express.static("public"));
 
-//Templating Engine
+// Templating Engine
 app.use(expressLayout);
 app.set("layout", "./layouts/main");
 app.set("view engine", "ejs");
