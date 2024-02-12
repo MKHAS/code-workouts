dac3ba8dd661f0720d8407846b23d4cd02d247c4
Mohamad-Kazem Abd-el-Sater <mkhas313@gmail.com>
2024-02-12 20:04:20 +0200

    Add admin.js and some basic building blocks inside

---
+++ spikes/server/routes/admin.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const router = express.Router();
+const Post = require("../models/Post");
+
+/**
+ * GET /
+ * HOME
+ */
+
+router.get("/admin", async (req, res) => {
+	try {
+		const locals = {
+			title: "Admin",
+			description: "Simple Blog created with NodeJS, Express and MongoDB",
+		};
+		res.render("admin/index", { locals });
+	} catch (error) {
+		console.log(error);
+	}
+});
+
+router.get("/about", (req, res) => {
+	res.render("about");
+});
+
+module.exports = router;
