37e06c93d63645a666c11fa206d6d1e1332c38a6
Mohamad-Kazem Abd-el-Sater <mkhas313@gmail.com>
2024-02-13 21:22:32 +0200

    Implement basic registration, password hashing using bcrypt

--- spikes/app.js
+++ spikes/app.js
@@ -1,6 +1,9 @@
 const dotenv = require("dotenv").config();
 const express = require("express");
 const expressLayout = require("express-ejs-layouts");
+const cookieParser = require("cookie-parser");
+const MongoStore = require("connect-mongo");
+const session = require("express-session");
 
 const connectDB = require("./server/config/db");
 
@@ -13,6 +16,21 @@ connectDB();
 //Enable us to pass data through forms
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// Other middleware
+app.use(cookieParser());
+
+// Session settings
+app.use(
+	session({
+		secret: "keyboard cat",
+		resave: false,
+		saveUninitialized: true,
+		store: MongoStore.create({
+			mongoUrl: process.env.MONGODB_URI,
+		}),
+	})
+);
 
 // static is serving the public folder allowing us to use it from our ejs files
 app.use(express.static("public"));
--- spikes/server/routes/admin.js
+++ spikes/server/routes/admin.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const Post = require("../models/Post");
 const User = require("../models/User");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const adminLayout = "../views/layouts/admin";
 
@@ -17,6 +19,48 @@ router.get("/admin", async (req, res) => {
 			description: "Simple Blog created with NodeJS, Express and MongoDB",
 		};
 		res.render("admin/index", { locals, layout: adminLayout });
+	} catch (error) {
+		console.log(error);
+	}
+});
+
+/**
+ * POST /
+ * Admin - Check Login
+ */
+router.post("/admin", async (req, res) => {
+	try {
+		const { username, password } = req.body;
+		console.log(req.body);
+		res.redirect("/admin");
+	} catch (error) {
+		console.log(error);
+	}
+});
+
+router.get("/about", (req, res) => {
+	res.render("about");
+});
+
+/**
+ * POST /
+ * Admin - Register
+ */
+
+router.post("/register", async (req, res) => {
+	try {
+		const { username, password } = req.body;
+		// console.log(`Username: ${username}, Password: ${password}`);
+		const hashedPassword = await bcrypt.hash(password, 10);
+		try {
+			const user = await User.create({ username, password: hashedPassword });
+			res.status(201).json({ message: "User Created", user });
+		} catch (error) {
+			if (error === 11000) {
+				res.status(409).json({ message: "User already in use" });
+			}
+			res.status(500).json({ message: "Internal Server Error XD" });
+		}
 	} catch (error) {
 		console.log(error);
 	}
