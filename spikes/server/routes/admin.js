a748c88983f6eeb5ade6a6ee7e0dc12f134dfc08
Mohamad-Kazem Abd-el-Sater <mkhas313@gmail.com>
2024-02-22 14:58:16 +0200

    Implement Delete button functionality

--- spikes/server/routes/admin.js
+++ spikes/server/routes/admin.js
@@ -210,4 +210,18 @@ router.get("/edit-post/:id", authMiddleware, async (req, res) => {
 	}
 });
 
+/**
+ * DELETE
+ * Admin - Delete Post
+ */
+
+router.delete("/delete-post/:id", authMiddleware, async (req, res) => {
+	try {
+		await Post.deleteOne({ _id: req.params.id });
+		res.redirect("/dashboard");
+	} catch (error) {
+		console.log(error);
+	}
+});
+
 module.exports = router;
